1658a8961786eb35f7191f3d7587ea3e345f535e
Khalob Cognata <khalob@users.noreply.github.com>
2017-12-02 17:05:58 -0500

    Add files via upload

---
+++ cordova files/ChemStock/www/js/email-alert.js
@@ -0,0 +1,19 @@
+  var makerKey = 'bx2QzrtMAF5P0kCraFDdz8';
+  var eventName = 'send_email';
+
+  function triggerEmailAlert(email, subject, content) {
+    //var data = '?value1=' + email.replace(" ", "%20") + '&value2=' + subject.replace(" ", "%20") + '&value3=' + content.replace(" ", "%20");
+	
+    var urlBuilt = 'https://maker.ifttt.com/trigger/' + eventName + '/with/key/' + makerKey;
+    $.ajax({
+      url: urlBuilt,
+	  type: "POST",
+	 // dataType: 'jsonp',
+	  data: {
+		  'value1': email,
+		  'value2': subject,
+		  'value3': content,
+	  },
+	  
+    });
+  }
