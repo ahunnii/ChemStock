9035d15369afb005eb5e9b23990bbe814ec7996c
bfriebe <bfriebe@users.noreply.github.com>
2017-12-02 18:31:10 -0500

    fixed bad table name

--- backend files/editrecipe.php
+++ backend files/editrecipe.php
@@ -4,7 +4,7 @@ require_once('conn.php');
 
 if(isset($_POST['id']) && isset($_POST['name']) && isset($_POST['prepQuantity']) && isset($_POST['prepType']) && isset($_POST['experiment']) && isset($_POST['requiredChemicals']) && isset($_POST['formula']) && isset($_POST['prepProcedure']))
 {
-	$stmt = $db->prepare("UPDATE recipe SET name = ?, prepQuantity = ?, prepType = ?, experiment = ?, requiredChemicals = ?, formula = ?, prepProcedure = ? WHERE id = ?");
+	$stmt = $db->prepare("UPDATE recipes SET name = ?, prepQuantity = ?, prepType = ?, experiment = ?, requiredChemicals = ?, formula = ?, prepProcedure = ? WHERE id = ?");
 	
 	$id = $db->real_escape_string($_POST['id']);
 	$name = $db->real_escape_string($_POST['name']);
