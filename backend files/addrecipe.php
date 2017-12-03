a4d98c69a947a29330eeb1625ba69633d1e7b273
bfriebe <bfriebe@users.noreply.github.com>
2017-12-02 19:55:37 -0500

    fixed same issue as before on different files

--- backend files/addrecipe.php
+++ backend files/addrecipe.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 require_once("conn.php");
-if(isset($_POST['name']) && isset($_POST['prepQuantity']) && isset($_POST['prepType']) && isset($_POST['experiment']) && isset($_POST['requiredChemicals']) && isset($_POST{'formula']) && isset($_POST['procedure']))
+if(isset($_POST['name']) && isset($_POST['prepQuantity']) && isset($_POST['prepType']) && isset($_POST['experiment']) && isset($_POST['requiredChemicals']) && isset($_POST{'formula']) && isset($_POST['prepProcedure']))
 {
 	$name = $db->real_escape_string($_POST['name']);
 	$prepQuantity = $db->real_escape_string($_POST['prepQuantity']);
@@ -9,9 +9,9 @@ if(isset($_POST['name']) && isset($_POST['prepQuantity']) && isset($_POST['prepT
 	$experiment = $db->real_escape_string($_POST['experiment']);
 	$requiredChemicals = $db->real_escape_string($_POST['requiredChemicals']);
 	$formula = $db->real_escape_string($_POST['formula']);
-	$procedure = $db->real_escape_string($_POST['procedure']);
+	$procedure = $db->real_escape_string($_POST['prepProcedure']);
 	
-	$stmt = $db->prepare("INSERT INTO recipes (name, prepQuantity, experiment, requiredChemicals, formula, recipeProcedure) VALUES (?,?,?,?,?,?)";
+	$stmt = $db->prepare("INSERT INTO recipes (name, prepQuantity, experiment, requiredChemicals, formula, prepProcedure) VALUES (?,?,?,?,?,?)";
 	$stmt->bind_param('s', $name);
 	$stmt->bind_param('d', $prepQuantity);
 	$stmt->bind_param('s', $prepType);
