50d3ce1cdc02959cb3a231cf35821faf24655a08
bfriebe <bfriebe@users.noreply.github.com>
2017-12-02 23:30:03 -0500

    added missing parenthesese

--- backend files/addrecipe.php
+++ backend files/addrecipe.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 require_once("conn.php");
-if(isset($_POST['name']) && isset($_POST['prepQuantity']) && isset($_POST['prepType']) && isset($_POST['experiment']) && isset($_POST['requiredChemicals']) && isset($_POST{'formula']) && isset($_POST['prepProcedure']))
+if(isset($_POST['name']) && isset($_POST['prepQuantity']) && isset($_POST['prepType']) && isset($_POST['experiment']) && isset($_POST['requiredChemicals']) && isset($_POST['formula']) && isset($_POST['prepProcedure']))
 {
 	$name = $db->real_escape_string($_POST['name']);
 	$prepQuantity = $db->real_escape_string($_POST['prepQuantity']);
@@ -11,14 +11,8 @@ if(isset($_POST['name']) && isset($_POST['prepQuantity']) && isset($_POST['prepT
 	$formula = $db->real_escape_string($_POST['formula']);
 	$procedure = $db->real_escape_string($_POST['prepProcedure']);
 	
-	$stmt = $db->prepare("INSERT INTO recipes (name, prepQuantity, experiment, requiredChemicals, formula, prepProcedure) VALUES (?,?,?,?,?,?)";
-	$stmt->bind_param('s', $name);
-	$stmt->bind_param('d', $prepQuantity);
-	$stmt->bind_param('s', $prepType);
-	$stmt->bind_param('s', $experiment);
-	$stmt->bind_param('s', $requiredChemicals);
-	$stmt->bind_param('s', $formula);
-	$stmt->bind_param('s', $procedure);
+	$stmt = $db->prepare("INSERT INTO recipes (name, prepQuantity, experiment, requiredChemicals, formula, prepProcedure) VALUES (?,?,?,?,?,?)");
+	$stmt->bind_param('sdsssss', $name,$prepQuantity,$prepType,$experiment,$requiredChemicals,$formula,$procedure);
 	
 	if(!$stmt->execute())
 	{
