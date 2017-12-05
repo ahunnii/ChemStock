57594db4a6b425f57c9b5e7abb20491b89f1d096
bfriebe <bfriebe@users.noreply.github.com>
2017-12-05 17:36:55 -0500

    fixed bind_param in php

--- backend files/updateapplication.php
+++ backend files/updateapplication.php
@@ -9,8 +9,7 @@ if(!$_POST)
 if($_POST['id'] && $_POST['status'])
 {
 	$updateStmt = $db->prepare("UPDATE applications SET status = ? WHERE id = ?");
-	$updateStmt->bind_param('s',$newStatus);
-	$updateStmt->bind_param('i',$appID);
+	$updateStmt->bind_param('si',$newStatus, $appID);
 
 	$appID = $db->real_escape_string($_POST['id']);
 	$newStatus = $db->real_escape_string($_POST['status']);
@@ -24,10 +23,7 @@ if($_POST['id'] && $_POST['status'])
 	if($newStatus == "ACCEPTED")
 	{
 		$insertStmt = $db->prepare("INSERT INTO employees (firstName, lastName, email, phoneNumber) VALUES (?, ?, ?, ?)");
-		$insertStmt->bind_param('s', $firstName);
-		$insertStmt->bind_param('s', $lastName);
-		$insertStmt->bind_param('s', $email);
-		$insertStmt->bind_param('s', $phone);
+		$insertStmt->bind_param('ssss', $firstName,$lastName,$email,$phone);
 		
 		$firstName = $db->real_escape_string($_POST['firstName']);
 		$lastName = $db->real_escape_string($_POST['lastName']);
