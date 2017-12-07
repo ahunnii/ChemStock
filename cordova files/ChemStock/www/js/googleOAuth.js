dbf9abcfd1c0d81c4651022912dd369fe545832a
bfriebe <bfriebe@users.noreply.github.com>
2017-12-06 20:22:50 -0500

    Removed 'clickme' button and redirected login to main.html

--- cordova files/ChemStock/www/js/googleOAuth.js
+++ cordova files/ChemStock/www/js/googleOAuth.js
@@ -11,7 +11,7 @@ function googleLogin()
 		var provider = email.replace(/^.*@/,"");
 		if(provider === "emich.edu")
 		{
-			location.href = "calculator.html";
+			location.href = "main.html";
 		}
 		else
 		{
@@ -37,7 +37,7 @@ function googleSilentLogin()
 		var provider = email.replace(/^.*@/,"");
 		if(provider === "emich.edu")
 		{
-			location.href = "calculator.html";
+			location.href = "main.html";
 		}
 		else
 		{
